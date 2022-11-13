4bab23993e9fb3a8c1b89de5cd8c8ebe1433a9fa
Eric Pazder <EricPazder@hotmail.ca>
2022-11-12 18:56:49 -0800

    TestOne

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+  SerialHelloWorld
+  Sends Hello world and the users name over serial at a baud rate of 9600
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+*/
+
+#include "Arduino.h"                                        //Only part of platform io different from arduino ide
+
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);                                 // initialize serial communication at 9600 bits per second:
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  Serial.println("Hello World from Eric Pazder");     // Print out required information 
+  delay(10);                                          // delay in between reads for stability
+}
